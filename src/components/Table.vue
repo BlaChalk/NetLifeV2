d2e6deeb81f43858f98c076b62d3cfaa559890d6
BlaChalk <black00731@gmail.com>
2021-03-27 22:21:17 +0800

    + Entry@views & Nav&Table@components

---
+++ src/components/Table.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  #table
+    img(alt="" src="../assets/calculator.png")
+    img(alt="" src="../assets/chrome.png")
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="sass" scoped>
+  #table
+    display: flex
+    flex-direction: column
+    justify-content: start
+    align-items: start
+    height: 100%
+    width: 100%
+    background-color: #777
+    img
+      width: 80px
+      height: 80px
+      margin: 30px
+      margin-bottom: 0px
+</style>
---
+++ src/views/Entry.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  #entry
+    <Table/>
+    <Nav/>
+</template>
+
+<script>
+import Table from '@/components/Table.vue'
+import Nav from '@/components/Nav.vue'
+
+export default {
+  name: 'entry',
+  components: {
+      Table, Nav
+  }
+}
+</script>
+
+<style lang="sass">
+  #entry
+    border: 1px solid #000
+    height: 100vh
+    width: 100vw
+    background-color: #aaa
+</style>
