59d52b2cf5f5d3ef211e61d49548bf7ff7480560
BlaChalk <black00731@gmail.com>
2021-04-14 16:52:01 +0800

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #deploy.sh
 #!/usr/bin/env sh
+
 # 當發生錯誤時終止腳本運行
 set -e
+
 # 打包
 npm run build
 
@@ -13,6 +15,7 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://github.com/BlaChalk/NetLifeV2.git 分支為 gh-pages
-git push -f git@github.com:BlaChalk/NetLifeV2.git master:gh-pages
-#將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
+git push -f https://github.com/BlaChalk/NetLifeV2.git master:gh-pages
+#將dist資料夾中的內容推送至遠端的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
+
 cd ..
